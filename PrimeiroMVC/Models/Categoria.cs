1009f1925afd25d06ee309a1be8e8966d0e817a5
Felipe Queiroga <luisfelipequeiroga@hotmail.com>
2021-02-23 12:59:07 -0400

    Modelo ASP.NET Core MVC - ProdutosXCategoria (LocalDB)

---
+++ PrimeiroMVC/Models/Categoria.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PrimeiroMVC.Models
+{
+    public class Categoria
+    {
+        public int Id { get; set; }
+        [Display(Name = "Descrição Categoria")]
+        [Required(ErrorMessage = "O Campo descrição é obrigatorio")]
+        public  string Descricao { get; set; }
+        public List<Produto> Produtos  {get; set; }
+}
+}
---
+++ PrimeiroMVC/Models/Context.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using PrimeiroMVC.Models;
+
+namespace PrimeiroMVC.Models
+{
+    public class Context : DbContext
+    {
+        public DbSet<Categoria> Categoria { get; set; }
+        public DbSet<Produto> Produto { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=Cursomvc;Integrated Security=True");
+        }
+
+        public DbSet<PrimeiroMVC.Models.Produto> Produto_1 { get; set; }
+    }
+}
